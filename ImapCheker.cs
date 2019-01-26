6d5d347829df3cc2a1718938a448aabe8604dfb7
Troodi <troodi@bk.ru>
2019-01-26 23:35:10 +0300

    Update ImapCheker.cs

--- ImapCheker.cs
+++ ImapCheker.cs
@@ -3,13 +3,9 @@ using MailKit.Net.Imap;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 
 class ImapChecker
 {
